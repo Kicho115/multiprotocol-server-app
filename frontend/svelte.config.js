7ca0196f0605fe1393c32808357a4037d01ec852
Kicho115 <oscar.leonardo.ang@gmail.com>
2026-02-26 03:56:17 -0800

    plssss

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,9 +3,6 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
-		postcss: true
-	}),
 	kit: {
 		adapter: adapter({
 			edge: false,
